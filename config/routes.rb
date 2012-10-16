0c5a512960e752019039caa6376a5a8d8713cd44
Sidd Kaza <sidd.kaza@gmail.com>
2012-10-16 13:51:15 -0400

    new controller main

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Gittest4::Application.routes.draw do
+  get "main/index"
+
+  get "main/login"
+
+  get "main/logout"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
